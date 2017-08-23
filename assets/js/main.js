a32bb5acc19f4a2d86fb1686d0a262291ef1f575
serg <benzins@gmail.com>
2017-08-23 14:15:42 +0300

    dexktop v8

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,11 +8,28 @@ document.addEventListener('DOMContentLoaded', function() {
     var setFlag = document.querySelector('[data-set-flag]');
     var setFlagContainer = document.querySelector('[data-set-flag-container]');
     var anchorButtons = document.querySelectorAll('[data-go-button]');
+    var form = document.querySelector('[data-send-form]');
     var i = 0;
 
-    setFlagContainer.addEventListener('click', function() {
+    function validation(type, value) {
+        switch (type) {
+            case 'text':
+                return /([^\s*$])/.test(value);
+                break;
+            case 'email':
+                return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(value);
+                break;
+        }
+    }
+
+    flagContainer.addEventListener('mouseover', function() {
         flagList.classList.add('open');
         setFlagContainer.classList.add('hide');
+    });
+
+    flagContainer.addEventListener('mouseout', function() {
+        setFlagContainer.classList.remove('hide');
+        flagList.classList.remove('open');
     });
 
     for (i = 0; i < flags.length; i++) {
@@ -36,6 +53,30 @@ document.addEventListener('DOMContentLoaded', function() {
             });
         });
     }
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        var formOk = true;
+        var inputs = this.querySelectorAll('[data-validate]');
+
+        for (i = 0; i < inputs.length; i++) {
+            inputs[i].parentNode.classList.remove('error');
+        }
+
+        for (i = 0; i < inputs.length; i++) {
+            var type = inputs[i].getAttribute('data-validate');
+            var value = inputs[i].value;
+            if (!validation(type, value)) {
+                formOk = false;
+                inputs[i].parentNode.classList.add('error');
+            }
+        }
+        if (formOk) {
+            return true;
+        }
+
+        return false;
+    })
 
 });
 
